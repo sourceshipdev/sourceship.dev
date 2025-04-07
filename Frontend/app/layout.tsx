c1378280a89951f11ae9b9e4cc10709ce3aa3a49
cadeci <ciaracade03@gmail.com>
2025-04-07 05:00:58 -0400

    feat: add vercel speedinsights

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({
         >
           {children}
           <Analytics />
+          <SpeedInsights/>
         </ThemeProvider>
       </body>
     </html>
